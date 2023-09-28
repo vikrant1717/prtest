f32a320811ab7b09549855bbc39b255d89a8ff6a
vikrant-phadtare1101 <vikrant.phadtare@embold.io>
2023-09-28 17:50:53 +0530

    "Commit :12"

---
+++ Commit12.java
@@ -0,0 +1,10 @@
+public class Commit12 {
+
+	public static void main(String args[]) {
+		try {
+			System.out.println("Screenshot taken for test : ");
+		} catch (Exception e) {
+			System.out.println("Exception " + e.getMessage());
+		}
+	}
+}
