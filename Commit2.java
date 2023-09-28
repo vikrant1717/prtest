304ee8b3a4387b9fc27028a5d91b91108659bb2c
vikrant-phadtare1101 <vikrant.phadtare@embold.io>
2023-09-28 17:50:53 +0530

    "Commit :2"

---
+++ Commit2.java
@@ -0,0 +1,10 @@
+public class Commit2 {
+
+	public static void main(String args[]) {
+		try {
+			System.out.println("Screenshot taken for test : ");
+		} catch (Exception e) {
+			System.out.println("Exception " + e.getMessage());
+		}
+	}
+}
