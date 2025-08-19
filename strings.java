d162df795d484d9707d8d9ee2a5931c96a7ea66a
vikrant1717 <vikrantphadtare9@gmail.com>
2025-08-19 09:05:48 +0530

    Automated file changes for load test

---
+++ strings.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class strings {
+
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter name:");
+//        String name = sc.nextLine();
+//        System.out.println();
+//        System.out.println("Full Name is: " + name);
+//
+//        System.out.println("length of string is" + " " + name.length());
+//
+//        System.out.println(name.charAt(4));
+
+//        String name1="vikrant";
+//        String name2="neha";
+//
+//        if (name1.compareTo(name2)>1)
+//        {
+//
+//            System.out.println("strings are equal");
+//        } else
+//        {
+//            System.out.println("strings are not equal");
+//
+//        }
+
+
+      String sentence = "my name is neha";
+       String name = sentence.substring(10);
+
+       System.out.println(name);
+
+    }
+}
