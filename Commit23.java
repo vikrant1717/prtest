6d3ac74427db5dde564b1b0266c776804f427493
vikrant11 <vikrantphadtare9@gmail.com>
2025-10-31 15:26:18 +0530

    Update Commit23.java

--- Commit23.java
+++ Commit23.java
@@ -3,8 +3,13 @@ public class Commit23 {
 	public static void main(String args[]) {
 		try {
 			System.out.println("Screenshot taken for test : ");
+			System.out.println("Screenshot taken for test : ");
+			System.out.println("Screenshot taken for test : ");
+			System.out.println("Screenshot taken for test : ");
+			System.out.println("Screenshot taken for test : ");
 		} catch (Exception e) {
 			System.out.println("Exception " + e.getMessage());
 		}
 	}
+
 }
